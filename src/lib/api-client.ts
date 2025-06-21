9962177d1e6e4b80be37693d48028107bce1378d
amit-amsl <202411926+amit-amsl@users.noreply.github.com>
2025-06-21 18:44:14 +0300

    Adjust API client for deployed backend

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 //http://localhost:3000/api
 //http://192.168.68.108:3000/api
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_APP_API_URL,
 });
 
 api.interceptors.response.use(
